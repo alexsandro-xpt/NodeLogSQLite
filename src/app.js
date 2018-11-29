6f90752448213178fafc5fbd7ee52e586814df14
Alexsandro Souza Pereira <alexsandro.xpt@gmail.com>
2018-11-28 22:59:57 -0200

    Add CORS support

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ const personRoute = require('./routes/relatorioRoute');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', index);
 app.use('/relatorio', personRoute);
 
